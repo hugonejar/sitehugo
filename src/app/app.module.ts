7e8678b3e324ce18fefa602373f7384ae0975637
Hugo Nejar Lopes <hugo.lopes@mottu.com.br>
2022-05-27 18:32:21 -0300

    commit primeiros passos cirando o heroi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
